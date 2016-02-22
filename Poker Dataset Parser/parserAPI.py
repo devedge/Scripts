75a4af8c3683ad0cc1dfe74ee8f3306b998fd8cd
devedge <devedge@outlook.com>
2016-02-21 23:45:10 -0500

    updated pokerparser array

--- Poker Dataset Parser/parserAPI.py
+++ Poker Dataset Parser/parserAPI.py
@@ -25,11 +25,11 @@ example1 =  [
                 [
                     "ZUf4UYlizITQF4Jk5sMf1A",   # player1 ID
                     1041,                       # dollars in chips
-                    ["Raises", [30, 30]],       # [action taken, value (if applicable)]
-                    ["Checks", "NULL"],
-                    ["Checks", "NULL"], 
-                    ["Checks", "NULL"], 
-                    ["Mucks", "NULL"]
+                    [["Raises", [30, 30]]],       # pocketCards       [action taken, value (if applicable)]
+                    [["Checks", "NULL"]],         # flop
+                    [["Checks", "NULL"]],         # turn
+                    [["Checks", "NULL"]],         # river
+                    [["Mucks", "NULL"]]           # showdown
                 ], 
                 [
                     "bSDJwbTmGA4R+u3vEJdXIA",
@@ -85,128 +85,72 @@ example2 =  [
                     False,      # only collected
                     1,          # number of winners
                     [1],        # index of winners
-                    [502]       # money received
+                    [502],      # money received
+                    ""          # winning hand (the full string)
                 ],
                 [
                     True,       # is there a loser      # lost info
                     1,          # number of losers
-                    [3]         # index of losers
+                    [3],        # index of losers
+                    ""          # losing hand (the full string)
                 ],
                 [
                     "y/xXiv3wrsVNOi4N7qXbuQ",   # player1 ID
                     2898,                       # dollars in chips
-                    ["Raises", [35, 35]],       # [action taken, value (if applicable)]
-                    ["Bets", 75],
-                    ["Bets", 140], 
-                    ["Checks", "NULL"], 
-                    ["Shows", ["10s", "Ad"]]
+                    [["Raises", [35, 35]]],       # [action taken, value (if applicable)]
+                    [["Bets", 75]],
+                    [["Bets", 140]], 
+                    [["Checks", "NULL"]], 
+                    [["Shows", ["10s", "Ad"]], ["Collects", 502]]
                 ], 
                 [
                     "9nEoiyBNT/5DNeziCrBkEw",
                     2164,
-                    ["Folds", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"]
+                    [["Folds", "NULL"]], 
+                    [["NA", "NULL"]], 
+                    [["NA", "NULL"]], 
+                    [["NA", "NULL"]], 
+                    [["NA", "NULL"]]
                 ],
                 [
                     "T2Y2AuJwSBvEYdoHMU4m1A",
                     2874.50,
-                    ["Calls", 25], 
-                    ["Checks", "NULL"], 
-                    ["Checks", "NULL"], 
-                    ["Checks", "NULL"], 
-                    ["Shows", ["10d", "10h"]]
-                ],
-                [
-                    "X+u4T/E5ANkyZLKm1YjqwQ",
-                    2468,
-                    ["Folds", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"]
+                    [["Calls", 25]], 
+                    [["Checks", "NULL"], ["Calls", 75]], 
+                    [["Checks", "NULL"], ["Calls", 140]], 
+                    [["Checks", "NULL"]], 
+                    [["Shows", ["10d", "10h"]]]
                 ],
                 [
                     "m5CQJNykYZdkaA89KwHNwQ",
-                    2537.50,
-                    ["Folds", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"]
-                ]
-            ]
-
-
-example3 =  [   
-                3065607844,     # game ID
-                "2009-07-14",   # date
-                "19:34:54(ET)", # time and timezone
-                5,              # number of rounds
-                ["Ac", "Jc", "9d", "10s", "4s"],   # cards on board
-                90,             # total pot amount
-                5,              # num players
-                1,              # dealer player
-                1,              # small blind player
-                2,              # big blind player
-                [
-                    True,       # game won              # win info
-                    False,      # only collected
-                    2,          # number of winners
-                    [1, 2],     # winners
-                    [43.50, 43.50]  # money received
+                    2785,
+                    [["Folds", "NULL"]], 
+                    [["NA", "NULL"]], 
+                    [["NA", "NULL"]], 
+                    [["NA", "NULL"]], 
+                    [["NA", "NULL"]]
                 ],
                 [
-                    False       # is there a loser      # lost info
+                    "ZUf4UYlizITQF4Jk5sMf1A",
+                    1186,
+                    [["Folds", "NULL"]], 
+                    [["NA", "NULL"]], 
+                    [["NA", "NULL"]], 
+                    [["NA", "NULL"]], 
+                    [["NA", "NULL"]]
                 ],
-                [
-                    "ZUf4UYlizITQF4Jk5sMf1A",   # player1 ID
-                    1041,                       # dollars in chips
-                    ["Raises", [30, 30]],       # [action taken, value (if applicable)]
-                    ["Checks", "NULL"],
-                    ["Checks", "NULL"], 
-                    ["Checks", "NULL"], 
-                    ["Mucks", "NULL"]
-                ], 
                 [
                     "bSDJwbTmGA4R+u3vEJdXIA",
-                    1012,
-                    ["Calls", 25], 
-                    ["Checks", "NULL"], 
-                    ["Checks", "NULL"], 
-                    ["Checks", "NULL"], 
-                    ["Shows", ["9h", "8h"]]
-                ],
-                [
-                    "y/xXiv3wrsVNOi4N7qXbuQ",
-                    3401,
-                    ["Calls", 20], 
-                    ["Checks", "NULL"], 
-                    ["Checks", "NULL"], 
-                    ["Checks", "NULL"], 
-                    ["Shows", ["9c", "6c"]]
-                ],
-                [
-                    "X+u4T/E5ANkyZLKm1YjqwQ",
-                    2468,
-                    ["Folds", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"]
-                ],
-                [
-                    "m5CQJNykYZdkaA89KwHNwQ",
-                    2537.50,
-                    ["Folds", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"]
+                    992,
+                    [["Folds", "NULL"]], 
+                    [["NA", "NULL"]], 
+                    [["NA", "NULL"]], 
+                    [["NA", "NULL"]], 
+                    [["NA", "NULL"]]
                 ]
             ]
+
+
 
 gameIDIndex = 0
 dateIndex = 1
@@ -270,3 +214,11 @@ def getWinnerID(gameArray):
 #    do something to each player
 
 
+def getPlayerAction(playerID, playNumber):
+    print("yeye")
+    # if check return check
+    # if bet return bet and amount
+    # if call return call and amount
+    # if raise return raise and [from amount, to amount]
+    # if fold return fold
+    # if muck return muck
