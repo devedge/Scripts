e52dc49d67cfa7a6d4801c6bbc5c4e729d364375
devedge <devedge@outlook.com>
2016-02-22 22:45:56 -0500

    more work done on dataset parser

--- Poker Dataset Parser/datafileParser.py
+++ Poker Dataset Parser/datafileParser.py
@@ -1,146 +1,327 @@
 
 import re
+# from decimal import Decimal
 
 # the file is parsed like a state machine (will have to do it anyway since every line needs to be parsed)
 # as each line is processed, counters and indices are updated to reflect the current state of the 
 #   lines being processed
 
+# small blinds are $5
+# big blinds are $10
+
 # player numbers are assigned in the order they appear
-# their IDs and number are stored in an array to quickly reference just over the current parsing
+# their IDs and number are stored in two different arrays to quickly reference just over the current parsing
 
-datafile = "/home/usr/dataset-test.txt"
+firstGame = True
+currentSector = "init"
+dealerSeat = 0
+tempPlayerArray = []
+tempPlayerIndex = []
+gamesList = []
+gameArray = []
+playerArrayInstance = []
 
-# c1 = 0
-# c2 = 0
-
-# numberGamesParsed = 0
-
-# def parseFile():
-
-gamearray = []
-initGameArray()
-
-with open(datafile, 'r') as f:
-
-
-    dealerSeat = 0
-
-    pocketCards = False
-    flop = False
-    turn = False
-    river = False
-    showdown = False
-
-    # Parse every line in the file
-    for line in f:
-
-        # This is the first line of a game, so (return the results of 
-        # the last game?) and reset all of the per-game variables.
-        # Then, extract the gameID, date, and time & timezone. 
-        if (re.search("Stage", line)):
-            # return the results of the last game?
-            # reset all the per-game variables
-            
-            dealerSeat = 0
-
-            line = line.split()
-            
-            # line[1] = int(line[1].replace("#", "").replace(":", ""))
-            
-            gamearray[0] = int(line[1].replace("#", "").replace(":", ""))   # insert game ID
-            gamearray[1] = line[7]                                          # insert date
-            gamearray[2] = (line[8] + line[9])                              # insert time and timezone
-
-            #gamearray.append(line[1])           # append game ID
-            #gamearray.append(line[7])           # append date
-            #gamearray.append(line[8] + line[9]) # append time and timezone
-            
-            # print(gamearray)
-            # print(line)
-            # c1 += 1
-            # go to a method that quits when the next game is found?
-        
-        # This is the second line of a new game, so extract and
-        # save the dealer seat number from the line.
-        elif (re.search("Table", line)):
-            line = line.split()
-
-            # doesn't work if the table has two-worded name (should work now)
-            dealerSeat = int(line[ len(line) - 4 ].replace("#", "")) 
-
-        # Use a REGEX to find the initial declaration of seats
-        elif (re.search("", line)):
-            
-            line = line.split()
-
-        # if pocket cards haven't started and the line contains "blind"
-
-        # elif (re.search("*** POCKET CARDS ***", line)):
-        # edits the first play array
-        
-        # elif (re.search("*** FLOP ***", line)):
-        # edits the second play array
-        
-        # elif (re.search("*** TURN ***", line)):
-        # edits the third play array
-
-        # elif (re.search("*** RIVER ***", line)):
-        # edits the fourth play array
-
-        # elif (re.search("*** SHOW DOWN ***", line)):
-        # edits the fifth play array
-
-        # elif (re.search("*** SUMMARY ***", line)):
-        # marks the end of the game
-
-        # elif (re.search("Total Pot", line)):
-        # 
-
-
-
-
+# Initializes a blank gameArray
 def initGameArray():
-    global gamearray
+    global gameArray
 
-    gamearray = [   
-                    0,       # game ID           (int)
-                    "NULL",  # date              (string)
-                    "NULL",  # time and timezone (string)
-                    False,   # game won          (boolean)
-                    0,       # number of rounds  (int)
-                    ["NULL", "NULL", "NULL", "NULL", "NULL"], # cards on board  (strings)
-                    0.0,     # total pot         (double)
-                    0.0,     # win amount        (double)
-                    0,       # number of players (int)
-                    0,       # dealer player     (int)
-                    0,       # small bind player (int)
-                    0,       # big bind player   (int)
-                    0,       # winning player    (int)
-                    [        # player 1 nested array
-                        "NULL",  # player ID        (string)
-                        0.0,     # dollars in chips (double)
-                        ["NULL", "NULL"], 
-                        ["NULL", "NULL"], 
-                        ["NULL", "NULL"], 
-                        ["NULL", "NULL"], 
-                        ["NULL", "NULL"]
-                    ], 
-                    [        # player 2 nested array
-                        "NULL",  # player ID        (string)
-                        0.0,     # dollars in chips (double)
-                        ["NULL", "NULL"], 
-                        ["NULL", "NULL"], 
-                        ["NULL", "NULL"], 
-                        ["NULL", "NULL"], 
-                        ["NULL", "NULL"]
+    gameArray = [
+                    0,              # 0  game ID
+                    "NULL",         # 1  date
+                    "NULL",         # 2  time and timezone
+                    0,              # 3  number of rounds
+                    [],             # 4  cards on board
+                    0.0,            # 5  total pot amount
+                    0,              # 6  num players
+                    0,              # 7  dealer player
+                    0,              # 8  small blind player
+                    0,              # 9  big blind player
+                    [
+                        False,      #  game won              # 10  win info
+                        False,      #  only collected
+                        0,          #  number of winners
+                        [],         #  index of winners
+                        [],         #  money received
+                        ""          #  winning hand (the full string?)
+                    ],
+                    [
+                        False,      #  is there a loser      # 11  lost info
+                        0,          #  number of losers
+                        [],         #  index of losers
+                        ""          #  losing hand (the full string?)
                     ]
-                    # etc... another nested array for each player
                 ]
 
+# Initializes a blank player array
+def initPlayerInstance():
+    global playerArrayInstance
+
+    playerArrayInstance =   [
+                                "NULL",  # player ID
+                                0.0,     # dollars in chips
+                                [["NA"]], 
+                                [["NA"]], 
+                                [["NA"]], 
+                                [["NA"]], 
+                                [["NA"]]
+                            ]
 
 
 
+file = "/home/usr/dataset-test.txt"
+
+
+def parseFile(datafile):
+
+    global firstGame
+    global tempPlayerArray
+    global tempPlayerIndex
+    global dealerSeat
+    global currentSector
+
+    initGameArray()
+    initPlayerInstance()
+
+    seatCounter = 0
+
+    with open(datafile, 'r') as f:
+        # pocketCards = False
+        # flop = False
+        # turn = False
+        # river = False
+        # showdown = False
+
+        # OR
+        # declare the current sector we're in and another statement will 
+        # currentSector = "init" # pocketCards, flop, turn, river, showdown, summary (need to initialize here?)
+        # numPlayers = 0
+
+        # Parse the file line by line
+        for line in f:
+
+            # This is the first line of a game, so (return the results of 
+            # the last game?) and reset all of the per-game variables.
+            # Then, extract the gameID, date, and time & timezone. 
+            if (re.search("Stage", line)):
+                # return the results of the last game?
+                
+
+                # reset all the per-game variables
+                # tempPlayerArray, tempPlayerIndex, playerArrayInstance, gameArray
+                tempPlayerArray = []
+                tempPlayerIndex = []
+                initPlayerInstance()
+                initGameArray()
+                
+                currentSector = "init"
+                dealerSeat = 0
+
+                line = line.split()
+                
+                gameArray[0] = int(line[1].replace("#", "").replace(":", ""))   # insert game ID
+                gameArray[1] = line[7]                                          # insert date
+                gameArray[2] = (line[8] + line[9])                              # insert time and timezone
+            
+            # This is the second line of a new game, so extract and
+            # save the dealer seat number from the line.
+            elif (re.search("Table", line)):
+                line = line.split()
+
+                # doesn't work if the table has two-worded name (should work now)
+                dealerSeat = int(line[ len(line) - 4 ].replace("#", "")) 
+
+            elif (re.search("\*\*\* POCKET CARDS \*\*\*", line)):
+                currentSector = "pocketCards"
+                gameArray[3] += 1
+            
+            elif (re.search("\*\*\* FLOP \*\*\*", line)):
+                currentSector = "flop"
+                gameArray[3] += 1
+            
+            elif (re.search("\*\*\* TURN \*\*\*", line)):
+                currentSector = "turn"
+                gameArray[3] += 1
+
+            elif (re.search("\*\*\* RIVER \*\*\*", line)):
+                currentSector = "river"
+                gameArray[3] += 1
+
+            elif (re.search("\*\*\* SHOW DOWN \*\*\*", line)):
+                currentSector = "showdown"
+                gameArray[3] += 1
+
+            elif (re.search("\*\*\* SUMMARY \*\*\*", line)):
+                currentSector = "summary"
+
+            # Depending on the current sector the loop is in, parse the user actions and 
+            # populate the gameArray
+            elif (len(line.split()) != 0):  # as long as the line is not empty
+                # If this is the "init" sector, first grab the seat initializations, then
+                # grab the blind posts
+                if (currentSector == "init"):
+                    splitline = line.split()
+
+                    # Extract the seat assignments, user IDs, and dollar amount in chips per
+                    # user
+                    if (re.search("Seat", line)): #splitline[0] == "Seat"
+                        # append an initialized playerArrayInstance to the gameArray
+                        initPlayerInstance()
+                        gameArray.append(playerArrayInstance)
+
+                        tempPlayerArray.append(splitline[3])
+                        tempPlayerIndex.append(int(splitline[1]))
+
+                        # Save the user ID and dollar amount in chips
+                        currentPlayer = tempPlayerArray.index(splitline[3]) + 1
+                        gameArray[11 + currentPlayer][0] = splitline[3]
+                        gameArray[11 + currentPlayer][1] = float(splitline[4].replace("(", "").replace("$", "").replace(",", ""))
+
+                    # Extract the blind post declarations
+                    else:
+                        if (re.search("small blind", line)):
+                            gameArray[8] = tempPlayerArray.index(splitline[0]) + 1
+                        else:
+                            gameArray[9] = tempPlayerArray.index(splitline[0]) + 1  # the big blind is always last
+
+                            # Since the players are all saved, update the gameArray's info
+                            # Save the number of players
+                            gameArray[6] = len(tempPlayerArray)
+
+                            # Set the dealer player
+                            gameArray[7] = tempPlayerIndex.index(dealerSeat) + 1
+
+
+                # Parse all of the actions taken during the Pocket Cards round
+                if (currentSector == "pocketCards"):
+                    extractUserAction(line.split(), 2)
+
+                if (currentSector == "flop"):
+                    extractUserAction(line.split(), 3)
+
+                if (currentSector == "turn"):
+                    extractUserAction(line.split(), 4)
+
+                if (currentSector == "river"):
+                    extractUserAction(line.split(), 5)
+
+                if (currentSector == "showdown"):
+                    extractUserAction(line.split(), 6)
+
+                # If this is currently the summary, get the total pot, board results (if any),
+                # and the user results
+                if (currentSector == "summary"):
+                    # Extract the total pot amount from the line
+                    if (re.search("Total Pot", line)):
+                        line = line.split()
+
+                        gameArray[5] = float(line[1].replace("Pot($", "").replace(")", ""))
+
+                    # Extract the board configuration from the line
+                    elif (re.search("Board", line)):
+                        line = line.split()
+
+                        for x in range(1, len(line)):
+                            gameArray[4].append(line[x].replace("[", "").replace("]", ""))
+
+                    # Else parse the user results
+                    elif (re.search("Seat", line)): 
+                        # search for the word "Seat", and keep a count of the number of seats, 
+                        # since there is no indication of when the game is over
+                        seatCounter += 1
+                        # print("help")
+                        # print(gameArray[6])
+                        # print(seatCounter)
+                        line = line.split()
+
+                        # find out who won, who lost, who collected, and any other info that might be needed
+
+                        if (seatCounter == gameArray[6]):
+                            seatCounter = 0
+                            # print(gameArray)
+                            gamesList.append(gameArray)
+                            # if not firstGame:
+                            #     gamesList.append(gameArray)
+                            #     # print(gameArray)
+                            # else:
+                            #     firstGame = False
+                   
+    somevalue = 2
+    print(" ")
+    # print(gamesList[0])
+    # print(" ")
+    # print(gamesList[1])
+    # print(" ")
+    print(gamesList[somevalue][0:10])
+    print(" ")
+    print(gamesList[somevalue][10])
+    print(" ")
+    print(gamesList[somevalue][11])
+    print(" ")
+    print(gamesList[somevalue][12])
+    print(" ")
+    print(gamesList[somevalue][13])
+    print(" ")
+
+    print(gamesList[somevalue][14])
+    print(" ")
+    print(gamesList[somevalue][14][2])
+    print(gamesList[somevalue][14][3])
+    print(gamesList[somevalue][14][4])
+    print(gamesList[somevalue][14][5])
+    print(gamesList[somevalue][14][6])
+
+    print(" ")
+
+    print(gamesList[somevalue][15])
+    print(" ")
+    print(gamesList[somevalue][16])
+    print(" ")
+    # print(gamesList[somevalue][16])
+    # print(" ")
+    # print(gamesList[3])
+    print(" ")
+
+
+def extractUserAction(line, sectorIDX):
+
+    action = line[2]
+    actionEntry = []
+
+    if (action == "Bets"):
+        actionEntry.append("Bets")
+        actionEntry.append(float(line[3].replace("$", "")))
+
+    elif (action == "Calls"):
+        actionEntry.append("Calls")
+        actionEntry.append(float(line[3].replace("$", "")))
+
+    elif (action == "Raises"):
+        actionEntry.append("Raises")
+        actionEntry.append([line[3].replace("$", ""), line[5].replace("$", "")])
+
+    elif (action == "returned"):
+        actionEntry.append("returned")
+        actionEntry.append(float(line[3].replace("($", "").replace(")", "")))
+
+    elif (action == "Shows"):
+        actionEntry.append("Shows")
+        actionEntry.append([line[3].replace("[", ""), line[4].replace("]", "")])
+
+    elif (line[1] == "Collects"):
+        actionEntry.append("Collects")
+        actionEntry.append(float(line[2].replace("$", "")))
+
+    elif (action == "Does"):
+        actionEntry.append("Does not show")
+
+    else:
+        actionEntry.append(action) # check, fold and muck
+
+    if(gameArray[11 + tempPlayerArray.index(line[0]) + 1][sectorIDX][0][0] == "NA"):
+        gameArray[11 + tempPlayerArray.index(line[0]) + 1][sectorIDX][0] = actionEntry
+    else:                        
+        gameArray[11 + tempPlayerArray.index(line[0]) + 1][sectorIDX].append(actionEntry)
 
 
 
-
+parseFile(file)
--- Poker Dataset Parser/parserAPI.py
+++ Poker Dataset Parser/parserAPI.py
@@ -1,160 +1,18 @@
- 
-example1 =  [   
-                3065607844,     # game ID
-                "2009-07-14",   # date
-                "19:34:54(ET)", # time and timezone
-                5,              # number of rounds
-                ["Ac", "Jc", "9d", "10s", "4s"],   # cards on board (can be variable)
-                90,             # total pot amount
-                5,              # num players
-                1,              # dealer player
-                1,              # small blind player
-                2,              # big blind player
-                [
-                    True,       # game won              # win info
-                    False,      # only collected
-                    2,          # number of winners
-                    [1, 2],     # index of winners
-                    [43.50, 43.50]  # money received
-                ],
-                [
-                    False,      # is there a loser      # lost info
-                    0,
-                    [0]
-                ],
-                [
-                    "ZUf4UYlizITQF4Jk5sMf1A",   # player1 ID
-                    1041,                       # dollars in chips
-                    [["Raises", [30, 30]]],       # pocketCards       [action taken, value (if applicable)]
-                    [["Checks", "NULL"]],         # flop
-                    [["Checks", "NULL"]],         # turn
-                    [["Checks", "NULL"]],         # river
-                    [["Mucks", "NULL"]]           # showdown
-                ], 
-                [
-                    "bSDJwbTmGA4R+u3vEJdXIA",
-                    1012,
-                    ["Calls", 25], 
-                    ["Checks", "NULL"], 
-                    ["Checks", "NULL"], 
-                    ["Checks", "NULL"], 
-                    ["Shows", ["9h", "8h"]]
-                ],
-                [
-                    "y/xXiv3wrsVNOi4N7qXbuQ",
-                    3401,
-                    ["Calls", 20], 
-                    ["Checks", "NULL"], 
-                    ["Checks", "NULL"], 
-                    ["Checks", "NULL"], 
-                    ["Shows", ["9c", "6c"]]
-                ],
-                [
-                    "X+u4T/E5ANkyZLKm1YjqwQ",
-                    2468,
-                    ["Folds", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"]
-                ],
-                [
-                    "m5CQJNykYZdkaA89KwHNwQ",
-                    2537.50,
-                    ["Folds", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"], 
-                    ["NA", "NULL"]
-                ]
-            ]
-
-example2 =  [   
-                3065446638,     # game ID
-                "2009-07-14",   # date
-                "18:49:11(ET)", # time and timezone
-                5,              # number of rounds
-                ["9d", "Ac", "7c", "Ah", "7h"],   # cards on board
-                505,             # total pot amount
-                6,              # num players
-                1,              # dealer player
-                2,              # small blind player
-                3,              # big blind player
-                [
-                    True,       # game won              # win info
-                    False,      # only collected
-                    1,          # number of winners
-                    [1],        # index of winners
-                    [502],      # money received
-                    ""          # winning hand (the full string)
-                ],
-                [
-                    True,       # is there a loser      # lost info
-                    1,          # number of losers
-                    [3],        # index of losers
-                    ""          # losing hand (the full string)
-                ],
-                [
-                    "y/xXiv3wrsVNOi4N7qXbuQ",   # player1 ID
-                    2898,                       # dollars in chips
-                    [["Raises", [35, 35]]],       # [action taken, value (if applicable)]
-                    [["Bets", 75]],
-                    [["Bets", 140]], 
-                    [["Checks", "NULL"]], 
-                    [["Shows", ["10s", "Ad"]], ["Collects", 502]]
-                ], 
-                [
-                    "9nEoiyBNT/5DNeziCrBkEw",
-                    2164,
-                    [["Folds", "NULL"]], 
-                    [["NA", "NULL"]], 
-                    [["NA", "NULL"]], 
-                    [["NA", "NULL"]], 
-                    [["NA", "NULL"]]
-                ],
-                [
-                    "T2Y2AuJwSBvEYdoHMU4m1A",
-                    2874.50,
-                    [["Calls", 25]], 
-                    [["Checks", "NULL"], ["Calls", 75]], 
-                    [["Checks", "NULL"], ["Calls", 140]], 
-                    [["Checks", "NULL"]], 
-                    [["Shows", ["10d", "10h"]]]
-                ],
-                [
-                    "m5CQJNykYZdkaA89KwHNwQ",
-                    2785,
-                    [["Folds", "NULL"]], 
-                    [["NA", "NULL"]], 
-                    [["NA", "NULL"]], 
-                    [["NA", "NULL"]], 
-                    [["NA", "NULL"]]
-                ],
-                [
-                    "ZUf4UYlizITQF4Jk5sMf1A",
-                    1186,
-                    [["Folds", "NULL"]], 
-                    [["NA", "NULL"]], 
-                    [["NA", "NULL"]], 
-                    [["NA", "NULL"]], 
-                    [["NA", "NULL"]]
-                ],
-                [
-                    "bSDJwbTmGA4R+u3vEJdXIA",
-                    992,
-                    [["Folds", "NULL"]], 
-                    [["NA", "NULL"]], 
-                    [["NA", "NULL"]], 
-                    [["NA", "NULL"]], 
-                    [["NA", "NULL"]]
-                ]
-            ]
 
 
-
+# indices in the gameArray
 gameIDIndex = 0
 dateIndex = 1
 timeIndex = 2
+numRoundsIndex = 3
+boardCardsIndex = 4
+totalPotIndex = 5
+numPlayersIndex = 6
+dealerIndex = 7
+smallBindIndex = 8
+bigBindIndex = 9
+gameWonIndex = 10
+gameLostIndex = 11
 
 
 # should initialize the gamearray here once, and provide a method to wipe it after use
