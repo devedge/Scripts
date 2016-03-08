f02751d14f576989fcfa3bffb97fac4d2592ed7b
devedge <devedge@outlook.com>
2016-03-07 21:51:27 -0500

    Added Duplicate File Indexer, a NodeJS script

---
+++ Duplicate File Indexer/README.md
@@ -0,0 +1,27 @@
+
+## Install
+
+After unzipping [the file](), run this in the directory:
+
+`$ npm install`
+
+to install the packages locally.
+
+
+
+## To Use:
+
+Save the older duplicate files from the filepath "/folder/path/here"
+```
+$ node dfi_index.js --folder /folder/path/here --older
+```
+
+Save the newer duplicate files from the filepath "/folder/path/here"
+```
+$ node dfi_index.js --folder /folder/path/here --newer
+```
+
+(Default) Saves the older duplicate files from the filepath "/folder/path/here"
+```
+$ node dfi_index.js --folder /folder/path/here
+```
---
+++ Duplicate File Indexer/dfi_index.js
@@ -0,0 +1,169 @@
+var commandLineArgs = require('command-line-args');
+var recursive = require('recursive-readdir');
+var pathmod = require('path');
+var fs = require('fs');
+
+// A Map to check for duplicates
+var fileMap = new Map();
+
+var duplicatesDir = 'duplicates/';
+var saveOlder = true;
+var printed = false;
+var rootpath = '';
+
+// node hashIndexer.js --folder /path/to/folder --older
+
+// Get the user input from the command line, then
+// parse the results and save them
+var cli = commandLineArgs([
+    {name: 'folder', alias: 'f', type: String, multiple: false},
+    {name: 'older', alias: 'o', type: Boolean},
+    {name: 'newer', alias: 'n', type: Boolean}
+
+]);
+
+var options = cli.parse();
+
+rootpath = options.folder;
+duplicatesDir = pathmod.join(pathmod.dirname(rootpath), duplicatesDir);
+
+if (options.newer) {
+    saveOlder = false;
+} else if (options.older) {
+    saveOlder = true;
+}
+
+
+
+// Iterate recursively over every folder and subfolder to get an array of file.
+// Then call the hashAndMap() function to determine duplicates
+recursive(rootpath, function (err, files) {
+
+    // If an error happens, print it
+    if (err) {console.log(err)};
+
+    // Print out the number of files that have been found
+    console.log("Parsing " + files.length + ' files');
+
+    // For every file in the array, pass it to the hashAndMap() function
+    for (var i = files.length - 1; i >= 0; i--) {
+        hashAndMap(files[i], 'sha1');
+    }
+
+});
+
+
+
+// Hashes the file and attempts to store it in a Map object.
+// If there is already an entry, that means the current file
+// is a duplicate, and it is moved to a 'duplicates' folder.
+function hashAndMap(path, hashtype, cb) {
+    var crypto = require('crypto');
+
+    var fd = fs.createReadStream(path);
+    var hash = crypto.createHash(hashtype);
+    hash.setEncoding('hex');
+
+    // When the hash is done
+    fd.on('end', function() {
+        hash.end();
+
+        // Save the result
+        var result = hash.read();
+
+        // If it doesn't exist in the Map, save it with the
+        // hash as the key and the filepath as the value for
+        // the (key, value) pairs.
+        if (typeof fileMap.get(result) === 'undefined') {
+            fileMap.set(result, path);
+
+        } else {
+
+            // There is a duplicate, so call the function that moves the 
+            // file to a 'duplicates' folder.
+            movefile(path, fileMap.get(result), function (err) {
+                if(err){
+                    console.log('Could not move \'' + path + '\' for some reason')
+                    console.log(err);
+                }
+            });
+        }
+
+        // If this is a callback, return (error, data)
+        if (cb) {
+            cb(null, result);
+        }
+    });
+
+    // Pipe the file to the hash
+    fd.pipe(hash);
+};
+
+
+
+// Moves one of the duplicate files ('path' or 'orig') into the 'duplicatesDir' 
+// folder. Also creates a log of the original and duplicate files in the 
+// 'duplicatesDir' folder.
+function movefile(path, orig, cb) {
+    var mkdirp = require('mkdirp');
+
+    if (!printed) {console.log("Duplicates found. Saved at: " + duplicatesDir); printed = true;}
+
+    // make the directory
+    mkdirp(duplicatesDir, function(err) {
+
+        filebydate(path, orig, saveOlder, function (returnedFile) {
+
+            // move the file
+            fs.rename(returnedFile, pathmod.join(duplicatesDir, pathmod.basename(returnedFile)), function(err) {cb(err)});
+
+            // append the duplicate error to a log
+            var logfile = pathmod.join(duplicatesDir, 'duplicatelog.txt');
+
+            // messy code to format logfile entries
+            var m1 = 'Duplicate (orig) '
+            var m2 = '          (dupl) '
+
+            if (returnedFile === orig) {
+                m1 = m1 + '(moved): ' + orig + '\n';
+                m2 = m2 + '       : ' + path + '\n';
+                
+            } else {
+                m1 = m1 + '       : ' + orig + '\n';
+                m2 = m2 + '(moved): ' + path + '\n';
+            }
+
+            var message = m1 + m2 + '\n';
+            
+            // append it to a log
+            fs.appendFile(logfile, message, function(err) {cb(err)});
+        });
+    });
+}
+
+
+
+// Returns the oldest file. If 'getOlder' is true,
+// return the oldest file, and if it is 'false' return
+// the newer file
+function filebydate(file1, file2, getOlder, cb) {
+
+    var older = '';
+    var newer = '';
+
+    // find the oldest file
+    if (fs.statSync(file1).birthtime.getTime() > fs.statSync(file2).birthtime.getTime()) {
+        older = file2;
+        newer = file1;
+    } else {
+        older = file1;
+        newer = file2;
+    }
+
+    // based on the 'getOlder' boolean value, return the appropriate file
+    if (getOlder) {
+        cb(older);
+    } else {
+        cb(newer);
+    }
+}
