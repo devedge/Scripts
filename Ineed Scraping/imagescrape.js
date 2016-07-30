884e908cec4a744df9975ca1fe43e43c2095e87b
devedge <devedge@outlook.com>
2016-07-30 15:29:52 -0400

    initial proto working

--- Ineed Scraping/imagescrape.js
+++ Ineed Scraping/imagescrape.js
@@ -1,20 +1,52 @@
-var ineed = require('ineed');
+const request = require('request');
+const mkdirp = require('mkdirp');
+const ineed = require('ineed');
+const fs = require('fs');
+
+var url = 'https://github.com/devedge';
 
 
+mkdirp.sync('Images');
 
 // Run the image collector on the url
-ineed.collect.images.from('https://github.com/devedge', function (err, response, result) {
+ineed.collect.images.from(url, function (err, response, result) {
     // body...
     if (err) { 
-        console.log('ERROR: ' + err) 
+        console.log('ERROR: ' + err);
     } else {
-        console.log(result);
+        fetch_links(result);
     }
 });
 
 
+// Iterate over all the images and download them
+function fetch_links(result) {
+    result.images.forEach(function (imgstring) {
+        console.log(imgstring.src);
+
+        var name = imgstring.src.match('[^/]*$')[0]
+                            .replace('&', '')
+                            .replace('<', '')
+                            .replace('>', '');
 
 
+        request({
+            uri: imgstring.src,
+            encoding: null
+        }, function (err, resp, data) {
+            if (!err && resp.statusCode === 200) {
+
+                fs.writeFile('Images/' + name, data, function (err) {
+                    if (err) { console.log(err); }
+                });
+
+            } else {
+                console.log('ERROR: ' + err);
+            }
+        });
+        
+    });
+}
 
 
 
