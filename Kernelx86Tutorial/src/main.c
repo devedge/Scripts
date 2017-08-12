ceea81c467688666b1b6612518c336d5f1bdffe3
devedge <devedge@outlook.com>
2017-08-11 21:06:05 -0400

    Genesis finished, but not booting

---
+++ Kernelx86Tutorial/src/main.c
@@ -0,0 +1,7 @@
+// main.c -- Defines the C-code kernel entry point, calls initialization routines.
+// Made for JamesM's tutorials
+
+int main(struct multiboot *mboot_ptr) {
+  // All initialization call will go here
+  return 0xDEADBABA;
+}
